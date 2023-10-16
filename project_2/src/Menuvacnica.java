eeeb606d6028687642fcb4e1eb87f2857f7369f8
blazeskibojan <148095416+blazeskibojan@users.noreply.github.com>
2023-10-16 17:23:05 +0200

    Add files via upload

---
+++ project_2/src/Menuvacnica.java
@@ -0,0 +1,13 @@
+
+public class Menuvacnica {
+
+	public static void main(String[] args) {
+int den=500;
+double evro=61.7;
+double x=den/evro;
+System.out.println("evrovodenar");
+System.out.println(x);
+
+	}
+
+}
